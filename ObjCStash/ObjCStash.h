617d6d7ce1a495dc398338393169559b1d4f627a
Alan <amorford@luxoft.com>
2018-03-10 07:22:31 -0800

    Add Framework Project

---
+++ ObjCStash/ObjCStash.h
@@ -0,0 +1,19 @@
+//
+//  ObjCStash.h
+//  ObjCStash
+//
+//  Created by Alan on 3/10/18.
+//  Copyright © 2018 Alan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ObjCStash.
+FOUNDATION_EXPORT double ObjCStashVersionNumber;
+
+//! Project version string for ObjCStash.
+FOUNDATION_EXPORT const unsigned char ObjCStashVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjCStash/PublicHeader.h>
+
+
